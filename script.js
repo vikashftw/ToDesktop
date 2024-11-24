e30d173189f44c7c0d0b0a8ff9d760b7198136ea
Vikash Mall <vikashmall4242@gmail.com>
2024-11-23 20:40:40 -0600

    fix hamburger icon

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const navDialog = document.getElementById('nav-dialog');
 
 function handleMenu() {
+    // what this does is: If the class 'hidden' is already present, it removes the class.
+    // If class 'hidden' is not present, then it adds the class 'hidden'.
+    // basically toggles between hidden and block classes
+    
     navDialog.classList.toggle('hidden');
 }
